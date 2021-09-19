ca5df0fb51643d3c6ca54938e83032f5313aa417
TheRedX <redprogamingtv@gmail.com>
2021-09-19 17:54:24 +0300

    changes

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const Discord = require("discord.js");
+const dotenv = require("dotenv");
+dotenv.config();
 const config = require("./config.js");
 const client = new Discord.Client({intents: [Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MEMBERS]});
 
@@ -8,4 +10,4 @@ client.on("ready", () => {
 client.on("message", (msg) => {
   console.log(msg.author.tag+": "+msg.content)
 });
-client.login(config.get("token"));
+client.login(config.get("token") ?? process.env.TOKEN);
